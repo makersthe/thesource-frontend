5dbee49561056904e65cd17aa1ea7b689edd9b96
CHANN <chann@chann.kr>
2020-04-26 02:46:41 +0900

    Update: Allow decorator and add mobx

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,22 @@
-const { override, fixBabelImports, addLessLoader } = require('customize-cra')
+const {
+  override,
+  fixBabelImports,
+  addLessLoader,
+  addDecoratorsLegacy,
+} = require('customize-cra')
 
-module.exports = override(
-  fixBabelImports('import', {
-    libraryName: 'antd',
-    libraryDirectory: 'es',
-    style: true,
-  }),
+module.exports = {
+  webpack: override(
+    addDecoratorsLegacy(),
+    fixBabelImports('import', {
+      libraryName: 'antd',
+      libraryDirectory: 'es',
+      style: true,
+    }),
 
-  addLessLoader({
-    javascriptEnabled: true,
-    modifyVars: { '@primary-color': '#1DA57A' },
-  }),
-)
+    addLessLoader({
+      javascriptEnabled: true,
+      modifyVars: { '@primary-color': '#1DA57A' },
+    }),
+  ),
+}
