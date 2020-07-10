aa0e0410a96809773cde8d11e8d4aa079453bc47
JungHun Lee <jhoon2816@gmail.com>
2020-07-10 16:30:18 +0900

    Update: change css style

--- src/components/carousel/Banner.js
+++ src/components/carousel/Banner.js
@@ -6,14 +6,23 @@ import GnbSearch from '../header/GnbSearch'
 const Container = styled.div`
   width: 100%;
   height: 60vh;
+  margin-bottom: 50px;
   background: url(${bannerImg}) no-repeat;
   background-size: cover;
+  @media (max-width: 485px) {
+    max-width: 100%;
+    margin: 0 auto !important;
+  }
 `
 const SearchContainer = styled.div`
   display: flex;
   padding-top: 30vh;
   justify-content: center;
   aligne-items: center;
+  @media (max-width: 485px) {
+    max-width: 100%;
+    margin: 0 auto !important;
+  }
 `
 
 const Banner = () => {
